24622210e8eb21cfab10d6cd322e6cf0e83f827e
qianjiaqiu <qianjq@yusur.tech>
2024-02-22 17:43:56 +0800

    0.0.9 尝试解决报错问题 ReferenceError: itemId is not defined

--- my_test_mod_1/fishSpecial/setup.mjs
+++ my_test_mod_1/fishSpecial/setup.mjs
@@ -17,34 +17,25 @@ export function setup(ctx) {
             },
             {
                 type: "number",
-                name: "modification-range",
+                name: "modification-number",
                 label: "修改-范围",
                 min: -10,
                 max: 100,
                 default: 50,
             }]);
-
-            console.log(`配置对象 ${conf}`);
-        console.log(conf);
-
-
+        
         const buffEnabled = conf.get("mod-enabled");    // 是否启用buff
-        const buffMultiplier = conf.get("modification-range"); // 物品掉落率的倍增因子
+        const buffMultiplier = conf.get("modification-number"); // 钓鱼特殊物品掉率修改值
 
-        console.log(buffEnabled);
-        console.log(`是否启用buff ${buffEnabled}`);
         debugLog("debugLog", buffEnabled);
-        debugLog(`是否启用buff ${buffEnabled}`);
+        debugLog(`钓鱼特殊物品掉率修改值 ${buffMultiplier}`);
 
         ctx.onCharacterLoaded(async (ctx) => {
             // 测试生效时间
-            console.log(`onCharacterLoaded生效`);
             debugLog(`onCharacterLoaded生效`);
             const buffDropRates = game.modifiers.increasedFishingSpecialChance;
-            console.log(`全局特殊掉落率为 ${buffDropRates}`);
             debugLog(`全局特殊掉落率为 ${buffDropRates}`);
             const debuffDropRates = game.modifiers.decreasedFishingSpecialChance;
-            console.log(`特殊掉落率衰减为 ${debuffDropRates}`);
             debugLog(`特殊掉落率衰减为 ${debuffDropRates}`);
             // 监听钓鱼动作
             if (game.fishing.isActive) {
@@ -61,8 +52,7 @@ export function setup(ctx) {
                 // game.fishing.dropRates.set(itemId, newDropRate);
     
                 // 在控制台输出调试信息
-                console.log(`Buff生效：${itemId} 掉落率调整为 ${newDropRate}`);
-                debugLog(`Buff生效：${itemId} 掉落率调整为 ${newDropRate}`);
+                console.log(`Buff生效特殊物品掉落率调整为： ${newDropRate}`);
             }
         });
     });
